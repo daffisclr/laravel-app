1fbdeecbead7e6f27c9ca967a230620f4b175c57
daffisclr <daffisclr@gmail.com>
2024-06-08 20:22:32 +0700

    add user seeder

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,8 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->bigInteger('username')->unique()->nullable();
-            $table->bigInteger('phone')->nullable();
+            $table->string('username')->unique()->nullable();
+            $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role', ['Admin', 'Alumni', 'User'])->default('Alumni');
